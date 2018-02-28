2bfca0fb51684306c115372ac0a1eb7e15daea08
desmondBash <thejunkfolder@outlook.com>
2018-02-28 16:57:51 -0500

    yup

---
+++ cw/c05/rps/rock_paper_scissors.js
@@ -0,0 +1,64 @@
+// this app relies on the 'prompt' node module
+var prompt = require('prompt');
+
+// initializes game; prompts user for rock, paper, or scissors input
+function init () {
+	prompt.get('choice', function (err, result) {
+		var choice = result.choice;
+
+		// error handling - will run prompt again if user does not enter rock, paper, or scissors
+		if (choice !== 'rock' && choice !== 'paper' && choice !== 'scissors') {
+			return init();
+		}
+		// from des - try using Math.random to choose 1, 2, or 3 (rock, paper, or scissor)
+
+		console.log('The user selected: ' + choice + '!')
+
+		// triggers game
+		startGame(choice)
+	});
+}
+
+// turns on prompt; runs init
+prompt.start();
+init();
+
+function startGame(userChoice) {
+	var cpuChoice = generateCPUChoice();
+	var winner = compare(userChoice, cpuChoice);
+	
+// debugging from des	
+//	console.log('user choice ' + userChoice);
+//	console.log('cpu choice ' + cpuChoice);
+	
+	console.log(winner + ' is the winner!');
+}
+
+// do i really need to comment what this does
+function generateCPUChoice () {
+	var choiceArray = ['rock', 'paper', 'scissors']
+	var cpuChoice = choiceArray[Math.floor(Math.random() * choiceArray.length)]
+		return cpuChoice;
+}
+
+// sometimes there is a draw, doofus. nobodoy wins. just the way communists like it.
+function compare(userChoice, cpuChoice) {
+  if (userChoice === cpuChoice) {
+    return 'nobody';
+  } else if (userChoice === 'rock' && cpuChoice === 'paper') {
+      return 'cpu';
+  } else if (userChoice === 'rock' && cpuChoice === 'scissors') {
+      return 'user';
+  } else if (userChoice === 'paper' && cpuChoice === 'rock') {
+      return 'user';
+  } else if (userChoice === 'paper' && cpuChoice === 'scissors') {
+      return 'cpu';
+  } else if (userChoice === 'scissors' && cpuChoice === 'paper') {
+      return 'user';
+  } else if (userChoice === 'scissors' && cpuChoice === 'rock') {
+      return 'cpu';
+  }  
+}
+
+
+
---
+++ hw/c06/dog_hw.js
@@ -0,0 +1,44 @@
+/*
+
+Work with a partner to create a Dog constructor, which creates instances containing the following properties:
+
+* name (string)
+* species (string)
+* foodsEaten (array)
+
+And the following methods:
+
+* eatSomething(food): takes the 'food' argument (passed as a string) and pushes it into the 'foodsEaten' array
+* introduce: console.logs a string introducing itself, including its name, species, and what it has eaten.
+
+Create 3 dogs total. Make sure all 3 dogs have all properties set and methods defined.
+
+Exercise your dogs by retrieving their properties and using their methods. Practice using both syntaxes
+for retrieving properties (dot notation and brackets).
+
+*/
+
+var Dog = function(name, species) {
+	this.name = name;
+	this.species = species;
+	this.foodsEaten = []
+	this.eatSomething = function(food) {
+		this.foodsEaten.push(food);
+	}
+	this.introduce = function() {
+		console.log("my name is " + this.name + " i am a " + this.species + " and i ate these foods: " + this.foodsEaten)
+	}
+}
+
+var dog1 = new Dog('chucky', 'poodle')
+var dog2 = new Dog('bart', 'black lab')
+var dog3 = new Dog('fido', 'schnauzer')
+
+dog1.eatSomething('peaches')
+dog1.eatSomething('ravioli')
+
+
+//print something, dude
+dog1.introduce();
+// dog1.name = 'billybob'
+// dog1.introduce();
